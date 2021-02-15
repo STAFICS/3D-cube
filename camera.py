ec69f41fdf594baa08c9ab7dc461e82c3152bd10
Ильнар <iakhmadyan@gmail.com>
2021-02-15 06:20:29 +0500

    test commit1

--- camera.py
+++ camera.py
@@ -14,7 +14,7 @@ class Camera:
         self.near_plane = 0.1
         self.far_plane = 100
         self.moving_speed = 0.3
-        self.rotation_speed = 0.015
+        self.rotation_speed = 0.014
 
     def control(self):
         key = pg.key.get_pressed()
