f86d24e34205d8fdf7b243ba4c89ca92f51b47be
Ильнар <iakhmadyan@gmail.com>
2021-02-15 06:11:41 +0500

    test commit

--- object.py
+++ object.py
@@ -7,7 +7,8 @@ class Object:
         self.render = render
         self.vertexes = np.array([(0, 0, 0, 1), (0, 1, 0, 1), (1, 1, 0, 1), (1, 0, 0, 1),
                                   (0, 0, 1, 1), (0, 1, 1, 1), (1, 1, 1, 1), (1, 0, 1, 1)])
-        self.faces = np.array([(0, 1, 2, 3), (4, 5, 6, 7), (0, 4, 5, 1), (2, 3, 7, 6), (1, 2, 6, 5), (0, 3, 7, 4)])
+        self.faces = np.array([(0, 1, 2, 3), (4, 5, 6, 7), (0, 4, 5, 1),
+                               (2, 3, 7, 6), (1, 2, 6, 5), (0, 3, 7, 4)])
 
     def draw(self):
         self.screen_projection()
